a846835598449997be8b7885645f2dd117ee1d1a
Simon Schmitt <sls@sls.wtf>
2020-05-06 13:59:15 +0200

    responsiv header added

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
+from app import app
+from werkzeug.urls import url_parse
 from app import app
 
 
@@ -7,3 +9,5 @@ from app import app
 def index():
     title = "Home"
     return render_template('index.html', title=title)
+
+
